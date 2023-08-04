b1235fae9b97f13a7a44f4d7e7fe6d5113ba7635
Zaiyd Mala <zaiydhmala@gmail.com>
2023-08-04 18:58:20 -0400

    Add Profiles validation

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
+import { BrowseContainer } from '../containers/browse';
 
 export default function Browse() {
-    return (<p>Browse</p>)
+  // we need the series and films
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+
+  const slides = selectionFilter({ series, films });
+  console.log(slides);
+  // pass it to teh browse container
+  return <BrowseContainer slides={slides} />;
 }
